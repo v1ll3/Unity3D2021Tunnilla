b06e28e3fd85486e3da2e2a22aa4607f68941bdd
DESKTOP-7S77B5K\t430 <villeryhanen11@gmail.com>
2021-02-23 18:10:36 +0200

    23.2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
-    GameObject camera;
+    public GameObject camera;
 
     public float speed = 8f;
     public float gravity = -9.81f;
